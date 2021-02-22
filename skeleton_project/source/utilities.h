01a64b1d89e4318291c8b24924c48f78185031fd
gjermundbae <gjermundbae@hotmail.com>
2021-02-22 18:01:16 +0100

    Update utilities.h

--- skeleton_project/source/utilities.h
+++ skeleton_project/source/utilities.h
@@ -4,6 +4,6 @@
 #include "hardware.h"
 #include "state.h"
 
-int scanUp(int floor, struct State S);
-int scanDown(int floor);
+int scanUp(int floor,  struct State S);
+int scanDown(int floor,  struct State S);
 int order_isEmpty(struct State S);
